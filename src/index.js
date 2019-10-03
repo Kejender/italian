e2ab21db1953ee0fecd157d6e78e41f41059a653
Kejender <perttu.makkonen@gmail.com>
2019-10-03 13:30:40 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from 'react-bootstrap/Nav';
 import './index.css';
-//import './bootstrap.min.css';
 import dictionary from './italian.csv';
 import about from './about.md';
 
 const Navi = (props) => {
-
-/*
-onClick={props.toggleVisible}
-toggleVisible={this.toggleVisibleContent}
-*/
-
   return (
     <Nav variant="tabs" defaultActiveKey="home" toggleVisible={props.toggleVisibleContent} className="justify-content-left" >
     <Nav.Link href="#" onClick={props.toggleVisible} eventKey="home">Search</Nav.Link>
@@ -21,20 +14,16 @@ toggleVisible={this.toggleVisibleContent}
   );
 }
 
-
 const SearchDict = (props) => {
   return (
-    <div>
-    <div id="home">
-		<input type="text" id="searchfield" onKeyUp={props.onKeyUp} placeholder="Search for italian words"></input>
-    <div id="dict-ital"></div>
-    </div>
+    <div id="search">
+		  <input type="text" id="searchfield" onKeyUp={props.onKeyUp} placeholder="Search for italian words"></input>
+      <div id="dict-ital"></div>
     </div>
   );
 }
 
 const DictList = (props) => {
-
   let mastertable = props.value;
   console.log(mastertable.length);
 
@@ -43,7 +32,7 @@ const DictList = (props) => {
   }
 
   return (
-    <div id="home">
+    <div id="list">
     <div dangerouslySetInnerHTML={createMarkup()} />
     </div>
   );
@@ -78,18 +67,16 @@ class ItalianDictionary extends React.Component {
     }
     else {
         this.setState({
-            ...this.state,
-            isEmptyState: true,
-            isVisibleState: false
+          ...this.state,
+          isEmptyState: true,
+          isVisibleState: false
         })
-
     }
-
   }
 
   search(){
     console.log("search");
-    var input, filter, tabl, tablerow, firstcell, i, txtValue;
+    let input, filter, tabl, tablerow, firstcell, i, txtValue;
     input = document.getElementById('searchfield');
     filter = input.value.toUpperCase();
     tabl = document.getElementById("dict");
@@ -108,16 +95,6 @@ class ItalianDictionary extends React.Component {
 
   renderSearch(i) {
 
-    /*{this.state.isEmptyState && <SearchDict onKeyUp={() => this.search()} />}
-    {this.state.isEmptyState && <DictList value={this.state.dictionaryTable}/>}
-    {this.state.isVisibleState && <About value={this.state.about}/>}
-    
-    <SearchDict onKeyUp={() => this.search()} />
-    <DictList value={this.state.dictionaryTable}/>
-    <About value={this.state.about}/>
-    */
-
-
     return (
     <div>
     <Navi toggleVisible={this.toggleVisibleContent}/>
@@ -128,7 +105,6 @@ class ItalianDictionary extends React.Component {
     );
   }
 
-
   renderDict(i) {
     return (
       <div>
@@ -137,84 +113,55 @@ class ItalianDictionary extends React.Component {
     );
   }
 
-
 componentDidMount() {
-  
   let dictionaryTable = this.state.dictionaryTable;
   let abouttext = this.state.abouttext;
-  //let asia = this.state.asia;
   var masterlist = [];
-
-  console.log(about);
-
-  fetch(about)
+  
+  fetch(about) // md file imported in the beginning containing about-text
   .then(response => {
-    //console.log(response.text());
     return response.text();
   })
   .then(string => {
     console.log(string);
     abouttext = string;
     this.setState({abouttext});
-  });
+  }); // about
 
-  fetch(dictionary)
+  fetch(dictionary) // dictionary csv file
   .then(response => {
-  return response.text();
+    return response.text();
   })
   .then(string => {
-  var dict = string.split(",,");
-  console.log(dict.length);
+    let dict = string.split(",,");
+    console.log(dict.length);
 
   dict.forEach(el => {
-    var temprow;
+    let temprow;
     temprow = el.split(",");
     temprow = temprow.splice(1, 2);
 
     if (temprow[0].toUpperCase() !== temprow[1].toUpperCase()){
-        masterlist.push(temprow);
+      masterlist.push(temprow);
     }
     else{
-        console.log("SAMA "+temprow[0]);
+      console.log(`SAMA ${temprow[0]}`);
     }
   }); // dict foreach
 
   console.log(masterlist.length);
   masterlist.sort();
-  let table = document.createElement("table");
-  table = document.createElement("table");
-  table.setAttribute("id", "dict");
-  let row;
-
-
-  masterlist.forEach(el => {
-    //console.log("master "+ind);
-    row = table.insertRow();
-    let cell = row.insertCell();
-    cell.innerHTML = el[0];
-    cell = row.insertCell();
-    cell.innerHTML = el[1];
+  let texttable = "<table id=\"dict\"><tbody>";
+  masterlist.forEach(function(el) {
+    texttable = `${texttable} <tr><td>${el[0]}</td><td>${el[1]}</td></tr>`;
   });
+  return texttable;
 
-  return table;
-  })
-  .then(table => {
-
-    let texttable = "<table id=\"dict\"><tbody>";
-    masterlist.forEach(function(el) {
-      texttable = texttable +"<tr><td>"+el[0]+"</td><td>"+el[1]+"</td></tr>";
-    });
-
-    return texttable;
   }).then(texttable => {
-    texttable = texttable + "</tbody></table>";
+    texttable = `${texttable}</tbody></table>`;
     dictionaryTable = texttable;
     this.setState({dictionaryTable});
-
   });
-    
-    
-  // fetch then
 
 } // componentDidMount
 
@@ -226,7 +173,7 @@ componentDidMount() {
     );
   }
 
-} // 
+} // class ItalianDictionary 
 
 ReactDOM.render(
   <ItalianDictionary />,
